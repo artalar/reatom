8fc7459b298750206cee3333caaeaaf1f24aef8c
artalar <artalar@ya.ru>
2019-08-24 15:58:30 +0300

    lib/build/upd: configs, versions

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
-module.exports =
-  process.env.NODE_ENV === 'test'
-    ? {
-        presets: [
-          ['@babel/env', { targets: { node: '10' } }],
-          '@babel/preset-flow',
-          '@babel/typescript',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          '@babel/proposal-object-rest-spread',
-        ],
-      }
-    : {}
+const config = {
+  presets: ['@babel/typescript'],
+  plugins: ['@babel/plugin-proposal-class-properties'],
+}
+
+if (process.env.NODE_ENV === 'test') {
+  config.presets.push(
+    ['@babel/env', { targets: { node: '10' } }],
+    '@babel/react',
+  )
+  config.plugins.push('@babel/proposal-object-rest-spread')
+}
+
+module.exports = config
