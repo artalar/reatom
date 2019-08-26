799fd43de237bbe7e90423c94ec9efd6a5b451bc
artalar <artalar@ya.ru>
2019-08-26 10:02:01 +0300

    lib/docs/fix: wip warning

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-console.warn('REAtom is move to another package is name "reatom"')
+console.warn('REAtom still work in progress, do not use it in production')
 
 type ActionType = string
 type ActionTypesDictionary = { [key in ActionType]: true }
