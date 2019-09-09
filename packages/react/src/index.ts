94b7c76565042f4e6640f86d5e5e59ad20843426
artalar <artalar@ya.ru>
2019-09-09 07:01:16 +0300

    react/logic/mod: improvements

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -5,18 +5,21 @@ import {
   useState,
   useContext,
   useCallback,
+  MutableRefObject,
 } from 'react'
 
-import { Store, Atom, ActionCreator } from '@reatom/core'
+import { Store, Atom, ActionCreator, getIsAtom } from '@reatom/core'
+
+function noop() {}
 
 export const context = createContext<Store | null>(null)
 
-export function useForceUpdate() {
+function useForceUpdate() {
   const update = useState(0)[1]
   return useRef(() => update(v => v + 1)).current
 }
 
-function useUnsubscribe(ref: any) {
+function useUnsubscribe(ref: MutableRefObject<Function>) {
   useEffect(
     () => () => {
       ref.current()
@@ -25,33 +28,57 @@ function useUnsubscribe(ref: any) {
   )
 }
 
-export function useAtom<T>(atom: Atom<T>, silent = false): T {
+/**
+ * @param atom target atom for subscription
+ * @param isUpdatesNotNeeded usefully for mount lazy atoms, but not subscribe to it updates
+ * @returns atom value
+ */
+export function useAtom<T>(
+  atom: Atom<T> | (() => Atom<T>),
+  isUpdatesNotNeeded = false,
+): T {
   const atomRef = useRef<Atom<T>>()
   const stateRef = useRef<T>()
   const unsubscribeRef = useRef<Function>()
+  const isMountRef = useRef(false)
   const store = useContext(context) as Store
   const forceUpdate = useForceUpdate()
 
-  if (atomRef.current === undefined) {
-    atomRef.current = atom
-    unsubscribeRef.current = store.subscribe(atom, (state: any) => {
-      stateRef.current = state
-      if (!silent) forceUpdate()
-    })
+  if (!store) throw new TypeError('[reatom] The provider is not defined')
 
-    stateRef.current = store.getState(atom)
+  useEffect(() => {
+    isMountRef.current = true
+  }, [])
+
+  if (!atomRef.current) {
+    atomRef.current = getIsAtom(atom)
+      ? (atom as Atom<T>)
+      : (atom as () => Atom<T>)()
+    unsubscribeRef.current = store.subscribe(
+      atomRef.current,
+      isUpdatesNotNeeded
+        ? noop
+        : (state: any) => {
+            stateRef.current = state
+            if (isMountRef.current) forceUpdate()
+          },
+    )
+    stateRef.current = store.getState(atomRef.current)
   }
 
-  useUnsubscribe(unsubscribeRef)
+  useUnsubscribe(unsubscribeRef as MutableRefObject<Function>)
 
   return stateRef.current as T
 }
 
-export function useAction<T>(cb: ActionCreator<T>, deps: any[] = []) {
+export function useAction<T>(
+  cb: ActionCreator<T> | ((payload: T) => void),
+  deps: any[] = [],
+) {
   const store = useContext(context) as Store
 
   return useCallback((payload: T) => {
     const action = cb(payload)
-    if (typeof action === 'object' && action !== null) store.dispatch(action)
+    if (action) store.dispatch(action)
   }, deps)
 }
