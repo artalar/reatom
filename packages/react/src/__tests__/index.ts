d2c854e53aa1b3ce71c9934b7a7e70fe287d315e
artalar <artalar@ya.ru>
2019-09-09 07:01:33 +0300

    react/tests/add

--- packages/react/src/__tests__/index.ts
+++ packages/react/src/__tests__/index.ts
@@ -1,3 +1,20 @@
-describe('@reatom/core', () => {
-  test.skip('todo', () => {})
+import React from 'react'
+import { act } from 'react-dom/test-utils'
+import { render, fireEvent } from '@testing-library/react'
+import { declareAction, declareAtom } from '@reatom/core'
+import { context, useAtom } from '../index'
+
+const increment = declareAction()
+const countAtom = declareAtom(0, on => on(increment, state => state + 1))
+
+function Counter() {
+  return useAtom(countAtom)
+}
+
+describe('@reatom/react', () => {
+  test('should throw if provider is not set', () => {
+    expect(() => render(<Counter />)).toThrowError(
+      '[reatom] The provider is not defined',
+    )
+  })
 })
