0f4335e177c929a65c4b53cfcad4a2fd7f5aa3ab
artalar <artalar@ya.ru>
2019-09-24 10:49:59 +0300

    fix(react): #124 dynamic atom with deps

--- packages/react/src/__tests__/index.tsx
+++ packages/react/src/__tests__/index.tsx
@@ -1,33 +1,41 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { renderHook } from '@testing-library/react-hooks'
-import { act } from 'react-test-renderer';
-import { declareAction, declareAtom, createStore, Store, map } from '@reatom/core'
+import { act } from 'react-test-renderer'
+import {
+  declareAction,
+  declareAtom,
+  createStore,
+  Store,
+  map,
+} from '@reatom/core'
 import { context, useAtom, useAction } from '../index'
 
 const increment = declareAction()
 const add = declareAction<number>()
 const countAtom = declareAtom(['count'], 0, on => [
   on(increment, state => state + 1),
-  on(add, (state, payload) => state + payload)
+  on(add, (state, payload) => state + payload),
 ])
 
-function Provider(props: { store: Store, children?: any }) {
-  return (<context.Provider value={props.store}>{props.children}</context.Provider>)
+function Provider(props: { store: Store; children?: any }) {
+  return (
+    <context.Provider value={props.store}>{props.children}</context.Provider>
+  )
 }
 
 describe('@reatom/react', () => {
   test('throw Error if provider is not set', () => {
-    const { result }  = renderHook(() => useAtom(countAtom))
+    const { result } = renderHook(() => useAtom(countAtom))
     expect(result.error).toEqual(Error('[reatom] The provider is not defined'))
   })
 
   describe('useAtom', () => {
     test('returns atom state', () => {
       const store = createStore(countAtom)
-      const { result } = renderHook(() => useAtom(countAtom), {
-        wrapper: props => <Provider {...props} store={store} />
-      })
 
+      const { result } = renderHook(() => useAtom(countAtom), {
+        wrapper: props => <Provider {...props} store={store} />,
+      })
       expect(result.current).toBe(0)
     })
 
@@ -36,34 +44,40 @@ describe('@reatom/react', () => {
       const subscriber = jest.fn()
       store.subscribe = subscriber
       const { rerender } = renderHook(() => useAtom(countAtom), {
-        wrapper: props => <Provider {...props} store={store} />
+        wrapper: props => <Provider {...props} store={store} />,
       })
 
       rerender()
-      expect(subscriber.mock.calls.length).toBe(1)
+      expect(subscriber).toBeCalledTimes(1)
     })
 
     test('updates state after store change', () => {
       const store = createStore(countAtom, { count: 10 })
       const { result } = renderHook(() => useAtom(countAtom), {
-        wrapper: props => <Provider {...props} store={store} />
+        wrapper: props => <Provider {...props} store={store} />,
       })
 
-      act(() => { store.dispatch(increment()) })
+      act(() => store.dispatch(increment()))
       expect(result.current).toBe(11)
 
-      act(() => { store.dispatch(increment()) })
+      act(() => store.dispatch(increment()))
       expect(result.current).toBe(12)
     })
 
     test('updates dynamic atom state after props change', () => {
-      const store = createStore(countAtom, { count: 10 });
-      const { result, rerender } = renderHook(({
-        multiplier 
-      }) => useAtom(map(countAtom, count => count * multiplier)), {
-        initialProps: { multiplier: 2 }, 
-        wrapper: props => <Provider {...props} store={store} />
-      })
+      const store = createStore(countAtom, { count: 10 })
+      const { result, rerender } = renderHook(
+        ({ multiplier }) =>
+          useAtom(
+            useMemo(() => map(countAtom, count => count * multiplier), [
+              multiplier,
+            ]),
+          ),
+        {
+          initialProps: { multiplier: 2 },
+          wrapper: props => <Provider {...props} store={store} />,
+        },
+      )
 
       expect(result.current).toBe(20)
 
@@ -72,44 +86,44 @@ describe('@reatom/react', () => {
     })
 
     test('unsubscribes from previous dynamic atom', () => {
-      const store = createStore(countAtom, { count: 10 });
+      const store = createStore(countAtom, { count: 10 })
       const subscriber = jest.fn()
       const _subscribe = store.subscribe
       // @ts-ignore
-      store.subscribe = ((atom) => _subscribe(atom, subscriber))
+      store.subscribe = atom => _subscribe(atom, subscriber)
 
-      const { rerender } = renderHook(({
-        multiplier 
-      }) => useAtom(map(countAtom, count => count * multiplier)), {
-        initialProps: { multiplier: 2 }, 
-        wrapper: props => <Provider {...props} store={store} />
-      })
+      const { rerender } = renderHook(
+        ({ multiplier }) =>
+          useAtom(
+            useMemo(() => map(countAtom, count => count * multiplier), [
+              multiplier,
+            ]),
+          ),
+        {
+          initialProps: { multiplier: 2 },
+          wrapper: props => <Provider {...props} store={store} />,
+        },
+      )
 
-      act(() => { store.dispatch(increment()) })
-      expect(subscriber.mock.calls.length).toBe(1)
+      act(() => store.dispatch(increment()))
+      expect(subscriber).toBeCalledTimes(1)
 
       rerender({ multiplier: 3 })
 
       act(() => store.dispatch(increment()))
-      expect(subscriber.mock.calls.length).toBe(1)
+      expect(subscriber).toBeCalledTimes(2)
     })
 
     test('does not update state if flag "isUpdatesNotNeeded" is set', () => {
-      const store = createStore(countAtom, { count: 10 });
-      const { result, rerender } = renderHook(({
-        isUpdatesNotNeed
-      }) => useAtom(countAtom, isUpdatesNotNeed), {
-        initialProps: { isUpdatesNotNeed: false }, 
-        wrapper: props => <Provider {...props} store={store} />
+      const store = createStore(countAtom, { count: 10 })
+      const { result } = renderHook(() => useAtom(countAtom, true), {
+        wrapper: props => <Provider {...props} store={store} />,
       })
 
-      act(() => { store.dispatch(increment()) })
-      expect(result.current).toBe(11);
+      expect(result.current).toBe(10)
 
-      rerender({ isUpdatesNotNeed: true })
-
-      act(() => { store.dispatch(increment()) })
-      expect(result.current).toBe(11);
+      act(() => store.dispatch(increment()))
+      expect(result.current).toBe(10)
     })
 
     test('unsubscribes from store after unmount', () => {
@@ -117,19 +131,19 @@ describe('@reatom/react', () => {
       const _subscribe = store.subscribe
       const subscriber = jest.fn()
       // @ts-ignore
-      store.subscribe = ((atom) => _subscribe(atom, subscriber))
+      store.subscribe = atom => _subscribe(atom, subscriber)
 
       const { unmount } = renderHook(() => useAtom(countAtom, true), {
-        wrapper: props => <Provider {...props} store={store} />
+        wrapper: props => <Provider {...props} store={store} />,
       })
 
-      act(() => { store.dispatch(increment()) })
-      expect(subscriber.mock.calls.length).toBe(1)
+      act(() => store.dispatch(increment()))
+      expect(subscriber).toBeCalledTimes(1)
 
-      unmount();
+      unmount()
 
-      act(() => { store.dispatch(increment()) })
-      expect(subscriber.mock.calls.length).toBe(1)
+      act(() => store.dispatch(increment()))
+      expect(subscriber).toBeCalledTimes(1)
     })
   })
 
@@ -137,21 +151,22 @@ describe('@reatom/react', () => {
     test('returns binded action to dispatch', () => {
       const store = createStore(countAtom)
       const { result } = renderHook(() => useAction(increment), {
-        wrapper: props => <Provider {...props} store={store} />
+        wrapper: props => <Provider {...props} store={store} />,
       })
 
       expect(store.getState(countAtom)).toBe(0)
-      // TODO: result.current required `payload: undefined`
-      // @ts-ignore
       result.current()
       expect(store.getState(countAtom)).toBe(1)
     })
 
     test('returns binded action wrapper to dispatch', () => {
       const store = createStore(countAtom)
-      const { result } = renderHook(() => useAction((p: number) => add(p * 2)), {
-        wrapper: props => <Provider {...props} store={store} />
-      })
+      const { result } = renderHook(
+        () => useAction((p: number) => add(p * 2)),
+        {
+          wrapper: props => <Provider {...props} store={store} />,
+        },
+      )
 
       expect(store.getState(countAtom)).toBe(0)
 
@@ -164,20 +179,22 @@ describe('@reatom/react', () => {
       const dispatch = jest.fn()
       store.dispatch = dispatch
 
-      const { rerender, result } = renderHook(({ 
-        multiplier 
-      }) => useAction((count: number) => add(count * multiplier), [multiplier]), {
-        wrapper: props => <Provider {...props} store={store} />,
-        initialProps: { multiplier: 2 }
-      })
+      const { rerender, result } = renderHook(
+        ({ multiplier }) =>
+          useAction((count: number) => add(count * multiplier), [multiplier]),
+        {
+          wrapper: props => <Provider {...props} store={store} />,
+          initialProps: { multiplier: 2 },
+        },
+      )
 
-      result.current(10);
-      expect(dispatch.mock.calls[0][0].payload).toBe(20)
+      result.current(10)
+      expect(dispatch).toBeCalledWith(add(20))
 
       rerender({ multiplier: 3 })
 
-      result.current(10);
-      expect(dispatch.mock.calls[1][0].payload).toBe(30)
+      result.current(10)
+      expect(dispatch).toBeCalledWith(add(30))
     })
   })
 })
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -8,7 +8,7 @@ import {
   MutableRefObject,
 } from 'react'
 
-import { Store, Atom, ActionCreator, getIsAtom } from '@reatom/core'
+import { Store, Atom, ActionCreator, ActionType, Action } from '@reatom/core'
 
 function noop() {}
 
@@ -24,43 +24,48 @@ function useUnsubscribe(ref: MutableRefObject<Function>) {
     () => () => {
       ref.current()
     },
-    [ref],
+    [ref.current],
   )
 }
 
+const lifeCycleStatus = {
+  initActual: 'initActual',
+  initNotActual: 'initNotActual',
+  mounted: 'mounted',
+} as const
+
 /**
- * @param atom target atom for subscription
+ * @param atom target atom for subscription or callback for it creation
  * @param isUpdatesNotNeeded usefully for mount lazy atoms, but not subscribe to it updates
  * @returns atom value
  */
-export function useAtom<T>(
-  atom: Atom<T> | (() => Atom<T>),
-  isUpdatesNotNeeded = false,
-): T {
+export function useAtom<T>(atom: Atom<T>, isUpdatesNotNeeded = false): T {
   const atomRef = useRef<Atom<T>>()
   const stateRef = useRef<T>()
   const unsubscribeRef = useRef<Function>()
-  const isMountRef = useRef(false)
+  const isMountRef = useRef<keyof typeof lifeCycleStatus>(
+    lifeCycleStatus.initActual,
+  )
   const store = useContext(context) as Store
   const forceUpdate = useForceUpdate()
 
   if (!store) throw new TypeError('[reatom] The provider is not defined')
 
   useEffect(() => {
-    isMountRef.current = true
+    if (isMountRef.current === lifeCycleStatus.initNotActual) forceUpdate()
+    isMountRef.current = lifeCycleStatus.mounted
   }, [])
 
-  if (!atomRef.current) {
-    atomRef.current = getIsAtom(atom)
-      ? (atom as Atom<T>)
-      : (atom as () => Atom<T>)()
+  if (atomRef.current !== atom) {
+    atomRef.current = atom
     unsubscribeRef.current = store.subscribe(
       atomRef.current,
       isUpdatesNotNeeded
         ? noop
         : (state: any) => {
             stateRef.current = state
-            if (isMountRef.current) forceUpdate()
+            if (isMountRef.current === lifeCycleStatus.mounted) forceUpdate()
+            else isMountRef.current = lifeCycleStatus.initNotActual
           },
     )
     stateRef.current = store.getState(atomRef.current)
@@ -71,12 +76,25 @@ export function useAtom<T>(
   return stateRef.current as T
 }
 
-export function useAction<T>(
-  cb: ActionCreator<T> | ((payload: T) => void),
+type ActionBindedInferArguments<Payload> = Payload extends undefined
+  ? () => void
+  : (payload: Payload) => void
+type ActionBinded<Fn extends Function> = Fn extends ActionCreator<
+  infer Payload,
+  any
+>
+  ? ActionBindedInferArguments<Payload>
+  : Fn extends (...a: any[]) => Action<infer Payload> | undefined
+  ? ActionBindedInferArguments<Payload>
+  : never
+
+export function useAction<Fn extends Function>(
+  cb: Fn,
   deps: any[] = [],
-) {
+): ActionBinded<Fn> {
   const store = useContext(context) as Store
 
+  // @ts-ignore
   return useCallback((payload: T) => {
     const action = cb(payload)
     if (action) store.dispatch(action)
