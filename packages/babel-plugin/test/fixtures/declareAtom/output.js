0906c52dbc64786d417b88969d9899771754c88d
Wroud <wrouds@gmail.com>
2019-10-31 14:31:05 +0300

    fix(babel-plugin): missing TemplateLiteral recognition

--- packages/babel-plugin/test/fixtures/declareAtom/output.js
+++ packages/babel-plugin/test/fixtures/declareAtom/output.js
@@ -1,5 +1,6 @@
 import { declareAtom as a } from '@reatom/core';
 const myAtom0 = a("myAtom0", 'state', on => []);
 const myAtom1 = a('notMyAtom', 'state', on => []);
+const myAtomx = a(`notMyAtom`, 'state', on => []);
 const myAtom2 = a(['notMyAtom'], 'state', on => []);
 const myAtom3 = declareAtom('state', on => []);
