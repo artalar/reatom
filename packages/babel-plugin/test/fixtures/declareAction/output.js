1718642fc717bc8abedbe375db8bbbb4a4481659
daniil.bataev <daniil.bataev@sravni.ru>
2021-09-24 12:41:00 +0300

    chore: remove packages not migrate to v2

--- packages/babel-plugin/test/fixtures/declareAction/output.js
+++
@@ -1,9 +0,0 @@
-import { declareAction as a } from '@reatom/core';
-const myAction0 = a("myAction0");
-const myAction1 = a("myAction1", () => {});
-const myAction2 = a('notMyAction');
-const myAction3 = a(`notMyAction`);
-const myAction4 = a('notMyAction', () => {});
-const myAction5 = a(`notMyAction`, () => {});
-const myAction6 = a(['notMyAction'], () => {});
-const myAction7 = declareAction();
--- packages/babel-plugin/test/fixtures/map/code.js
+++
@@ -1,8 +0,0 @@
-import { map as m } from '@reatom/core'
-
-const counterDoubled0 = m(counter, value => value * 2)
-const counterDoubled1 = m('myName', counter, value => value * 2)
-const counterDoubledx = m(`myName`, counter, value => value * 2)
-const counterDoubled2 = m(['myName'], counter, value => value * 2)
-const mapFromArray = [].map(counter, value => value * 2)
-const counterDoubled3 = map(counter, value => value * 2)
