8ac804c6eff9ddbc84daf9e9fddb2ac4288e097e
daniilbataev <ragoga48@gmail.com>
2021-10-30 13:53:36 +0300

    refactor(cookie): remove draft unsubscribe when have not subscribers

--- packages/cookie/src/index.ts
+++ packages/cookie/src/index.ts
@@ -9,22 +9,19 @@ export const createCookieAtom = <T extends CookieObjectModel>(
   cookie: CookieController<T>,
   realTimeCookie: RealTimeCookie<T>,
 ) => {
-  const init = () => ({ payload: '', type: 'super-uniq' })
   type argsRemove = Parameters<CookieController<T>['remove']>
   type argsSet = Parameters<CookieController<T>['set']>
   return createAtom(
     {
-      init,
       set: (name: argsSet[0], value: argsSet[1], options?: argsSet[2]) => ({
         name,
         value,
         options,
       }),
-      _set: (cookieOnSet: Partial<T>) => cookieOnSet,
       remove: (name: argsRemove[0]) => name,
+      _set: (cookieOnSet: Partial<T>) => cookieOnSet,
     },
     ({ onAction, schedule, create, onInit }, cookieState: Partial<T> = {}) => {
-      console.log('reducer')
       let newCookie = cookieState
       onAction('set', ({ name, value, options }) => {
         schedule(() => {
@@ -39,41 +36,17 @@ export const createCookieAtom = <T extends CookieObjectModel>(
         schedule(() => {
           cookie.remove(name)
         })
-        newCookie = Object.fromEntries(
-          Object.entries(newCookie).filter(([key]) => key !== name),
-        ) as T
+        newCookie = { ...newCookie }
+        delete newCookie[name]
       })
       onInit(() => {
-        schedule((dispatch, ctx) => {
-          const handler = () => {
-            dispatch(init())
-            console.log('dispatch')
-            setTimeout(() => {
-              console.log('check', ctx.haveSubscribers)
-              if (!ctx.haveSubscribers) {
-                // @ts-ignore
-                realTimeCookie.removeListener(ctx.handler)
-                return
-              }
-              ctx.haveSubscribers = false
-              setTimeout(handler, 1000)
-            }, 100)
-          }
-          setTimeout(handler, 1000)
-        })
-        schedule((dispatch, ctx) => {
+        schedule((dispatch) => {
           dispatch(create('_set', cookie.get()))
-          const handler = (newCookieFromListener: Partial<T>) => {
-            dispatch(create('_set', newCookieFromListener))
-          }
           // @ts-ignore
-          realTimeCookie.addListener(handler)
-          ctx.handler = handler
+          realTimeCookie.addListener((newCookieFromListener: Partial<T>) => {
+            dispatch(create('_set', newCookieFromListener))
+          })
         })
-      })
-      schedule((dispatch, ctx) => {
-        console.log('subscribe true')
-        ctx.haveSubscribers = true
       })
       return newCookie
     },
