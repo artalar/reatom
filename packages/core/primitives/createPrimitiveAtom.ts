b1367fcecb7ce0ef2095bc5c9c987c9927745ed0
Alexander CherryTea <Akiyamka@gmail.com>
2022-02-23 20:43:42 +0300

    Update createPrimitiveAtom.ts

--- packages/core/primitives/createPrimitiveAtom.ts
+++ packages/core/primitives/createPrimitiveAtom.ts
@@ -60,7 +60,7 @@ export function createPrimitiveAtom<State>(
       payload(state),
   }
 
-  let { decorators = [], id } = isString(options)
+  let { decorators = [], store, id } = isString(options)
     ? ({ id: options } as Exclude<AtomOptions<State>, string>)
     : options
 
@@ -99,7 +99,7 @@ export function createPrimitiveAtom<State>(
 
     noop,
 
-    { decorators, id },
+    { decorators, store, id },
   )
 
   const actionCreatorsTypes = keys.map((key) => atom[key].type)
