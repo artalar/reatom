3e78d1a590e024241c05a2eedea6570a9c575738
artalar <artalar@ya.ru>
2022-06-28 14:51:03 +0300

    feat(logger): create package

---
+++ packages/logger/src/index.test.ts
@@ -0,0 +1,26 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import { atom, createContext } from '@reatom/core'
+import { mockFn } from '@reatom/internal-utils'
+
+import { connectLogger } from './'
+
+test(`base API`, async () => {
+  const a1 = atom(0)
+  const a2 = atom(0, `test`)
+  const ctx = createContext()
+  const log = mockFn()
+
+  connectLogger(ctx, { log })
+
+  ctx.run(() => {
+    a1(ctx, 1)
+    a2(ctx, 2)
+  })
+
+  assert.equal(log.lastInput(), { [a2.__reatom.name]: 2 })
+
+  1
+})
+
+test.run()
---
+++ packages/logger/src/index.ts
@@ -0,0 +1,21 @@
+import { Ctx, Rec } from '@reatom/core'
+
+export const connectLogger = (
+  ctx: Ctx,
+  { log = console.log }: { log?: typeof console.log } = {},
+) => {
+  return ctx.log((logs) => {
+    log(
+      logs.reduce((acc, patch) => {
+        if (
+          patch.meta.isInspectable &&
+          !Object.is(patch.state, ctx.read(patch.meta)?.state)
+        ) {
+          acc[patch.meta.name] = patch.state
+        }
+
+        return acc
+      }, {} as Rec),
+    )
+  })
+}
