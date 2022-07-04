17a52197f4015484772cea0fb048b56839965f3a
Arutyunyan Artyom <artalar@ya.ru>
2022-07-04 11:05:38 +0000

    feat(core): add experimental createTestStore

---
+++ packages/core/experiments/createTestStore.test.ts
@@ -0,0 +1,41 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import { createNumberAtom } from '@reatom/core/primitives'
+import { createAtom } from '@reatom/core'
+
+import { mockFn } from '../test_utils'
+
+import { createTestStore } from './createTestStore'
+
+test(`createTestStore`, async () => {
+  const a1 = createNumberAtom(1)
+  const a2 = createAtom({ a1 }, (track) => track.get(`a1`))
+  const a3 = createAtom({ a2 }, (track) => track.get(`a2`))
+  const store = createTestStore()
+  const cb = mockFn()
+
+  store.subscribe(a3, (newState) => cb(newState))
+
+  assert.is(store.getState(a1), 1)
+  assert.is(store.getState(a2), 1)
+  assert.is(store.getState(a3), 1)
+
+  store.dispatch(store.setState(a2, 2))
+  assert.is(store.getState(a1), 1)
+  assert.is(store.getState(a2), 2)
+  assert.is(store.getState(a3), 2)
+  ;`👍` //?
+})
+
+test(`createTestStore stale atom`, async () => {
+  const a1 = createNumberAtom(1)
+  const store = createTestStore()
+
+  store.dispatch(store.setState(a1, 123))
+
+  assert.is(store.getState(a1), 123)
+  ;`👍` //?
+})
+
+test.run()
