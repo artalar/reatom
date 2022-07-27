6c243543e5b5fcbb5792b0ec3b3da913fc72b1d2
artalar <artalar@ya.ru>
2022-07-27 15:52:46 +0300

    feat(logger): huge refactor

--- packages/logger/src/index.test.ts
+++ packages/logger/src/index.test.ts
@@ -1,24 +1,71 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
-import { atom, createContext } from '@reatom/core'
-import { mockFn } from '@reatom/internal-utils'
+import { action, atom, createContext } from '@reatom/core'
+import { mapAsync, toAtom } from '@reatom/lens'
+import { mockFn, sleep } from '@reatom/internal-utils'
 
-import { connectLogger } from './'
+import { connectLogger } from '.'
 
-test(`base API`, async () => {
+test(`base`, async () => {
   const a1 = atom(0)
   const a2 = atom(0, `test`)
   const ctx = createContext()
   const log = mockFn()
 
+  ctx.get(a1)
+  ctx.get(a2)
+
   connectLogger(ctx, { log })
 
-  ctx.run(() => {
+  ctx.get(() => {
     a1(ctx, 1)
     a2(ctx, 2)
   })
 
-  assert.equal(log.lastInput(), { [a2.__reatom.name!]: 2 })
+  assert.equal(log.lastInput().changes, {
+    [a2.__reatom.name!]: { cause: `self`, state: 2 },
+  })
+
+  ctx.get(() => {
+    a2(ctx, 10)
+    a2(ctx, 20)
+  })
+
+  assert.equal(log.lastInput().changes, {
+    [a2.__reatom.name!]: [
+      { cause: `self`, state: 10 },
+      { cause: `self`, state: 20 },
+    ],
+  })
+  ;`👍` //?
+})
+
+test(`cause`, async () => {
+  // should correct calculate cause for complex async transactions
+  const doAsync = action(
+    (ctx, v: number) => ctx.schedule(() => sleep(5).then(() => v)),
+    `doAsync`,
+  )
+  const asyncRes = doAsync.pipe(mapAsync((ctx, v) => v, `asyncRes`))
+  const a1 = asyncRes.pipe(toAtom(0, (ctx, v) => v, `a1`))
+
+  const ctx = createContext()
+  const log = mockFn()
+
+  connectLogger(ctx, { log })
+
+  ctx.subscribe(a1, () => {})
+
+  doAsync(ctx, 123)
+
+  await sleep(5)
+
+  assert.equal(log.lastInput().changes, {
+    [a1.__reatom.name!]: {
+      cause: `self <-- ${asyncRes.__reatom.name} <-- ${doAsync.__reatom.name}`,
+      state: [123],
+    },
+  })
   ;`👍` //?
 })
 
--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,21 +1,60 @@
-import { Ctx, Rec } from '@reatom/core'
+import { AtomCache, AtomMeta, Ctx, Rec } from '@reatom/core'
+
+const getName = ({ name = `unnamed` }: AtomMeta) => name
+
+const getCause = (patch: AtomCache) => {
+  let log = `self`
+  let cause: typeof patch.cause = patch
+
+  while (cause !== cause.cause && cause.cause !== null) {
+    cause = cause.cause
+    log += ` <-- ` + getName(cause.meta)
+  }
+
+  if (cause !== patch) log += ` <-- ` + getName(cause.meta)
+
+  return log
+}
 
 export const connectLogger = (
   ctx: Ctx,
-  { log = console.log }: { log?: typeof console.log } = {},
+  {
+    log = console.log,
+    showCause = true,
+  }: { log?: typeof console.log; showCause?: boolean } = {},
 ) => {
-  return ctx.log((logs) => {
-    log(
-      logs.reduce((acc, patch) => {
-        if (
-          patch.meta.name &&
-          !Object.is(patch.state, ctx.read(patch.meta)?.state)
-        ) {
-          acc[patch.meta.name] = patch.state
-        }
+  return ctx.subscribe((logs, error) => {
+    const counter = new Map<AtomMeta, number>()
+    const changes = logs.reduce((acc, patch) => {
+      const { meta, state } = patch
 
-        return acc
-      }, {} as Rec),
-    )
+      counter.set(meta, (counter.get(meta) ?? 0) + 1)
+
+      if (
+        meta.name &&
+        !Object.is(
+          state,
+          ctx.get((read) => read(meta)?.state),
+        )
+      ) {
+        const { name } = meta
+        const message = showCause ? { state, cause: getCause(patch) } : state
+
+        if (name in acc) {
+          if (counter.get(meta)! > 1) acc[name] = [acc[name]]
+          acc[name].push(message)
+        } else {
+          acc[name] = message
+        }
+      }
+
+      return acc
+    }, {} as Rec)
+
+    log({
+      changes,
+      logs,
+      error,
+    })
   })
 }
