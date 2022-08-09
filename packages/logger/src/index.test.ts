181a098a367d9914b4c3c06fe8893748adcec64a
artalar <artalar@ya.ru>
2022-08-10 00:03:16 +0300

    feat(logger): refactor, added createLogBatched

--- packages/logger/src/index.test.ts
+++ packages/logger/src/index.test.ts
@@ -2,9 +2,10 @@ import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 import { action, atom, createContext } from '@reatom/core'
 import { mapAsync, toAtom } from '@reatom/lens'
-import { mockFn, sleep } from '@reatom/internal-utils'
+import { sleep } from '@reatom/utils'
+import { mockFn } from '@reatom/testing'
 
-import { connectLogger } from '.'
+import { connectLogger, createLogBatched } from '.'
 
 test(`base`, async () => {
   const asyncResAtom = atom(0)
@@ -61,8 +62,73 @@ test(`cause`, async () => {
   await sleep(5)
 
   assert.equal(log.lastInput().changes, {
-    doAsyncRes: { state: [123], cause: 'self <-- doAsync' },
-    asyncResAtom: { state: 123, cause: 'self <-- doAsyncRes <-- doAsync' },
+    doAsyncRes: { state: [123], stateOld: [], cause: 'self <-- doAsync' },
+    asyncResAtom: {
+      state: 123,
+      stateOld: 0,
+      cause: 'self <-- doAsyncRes <-- doAsync',
+    },
+  })
+  ;`👍` //?
+})
+
+test(`should skip logs without state changes`, async () => {
+  const a = atom(0, 'nAtom')
+  const ctx = createContext()
+  const log = mockFn()
+
+  ctx.subscribe(a, () => {})
+
+  connectLogger(ctx, {
+    log: createLogBatched(log),
+  })
+
+  a(ctx, 1)
+
+  ctx.get(a)
+
+  a(ctx, 1)
+
+  assert.is(log.calls.length, 0)
+
+  await 0
+
+  assert.is(log.calls.length, 1)
+
+  a(ctx, 1)
+
+  assert.is(log.calls.length, 1)
+
+  await 0
+
+  assert.is(log.calls.length, 1)
+
+  a(ctx, 2)
+
+  ctx.get(() => {
+    ctx.get(a)
+  })
+
+  ctx.get(() => {
+    atom(0, 'nAtom1')(ctx, 1)
+    ctx.get(a)
+    atom(0, 'nAtom2')(ctx, 1)
+    a(ctx, 3)
+  })
+
+  a(ctx, 3)
+
+  assert.is(log.calls.length, 1)
+
+  await 0
+
+  assert.is(log.calls.length, 2)
+  log.lastInput() //?
+  assert.equal(log.lastInput(), {
+    '[1] nAtom': { state: 2, stateOld: 1, cause: 'self' },
+    '[2] nAtom1': { state: 1, stateOld: undefined, cause: 'self' },
+    '[2] nAtom2': { state: 1, stateOld: undefined, cause: 'self' },
+    '[2] nAtom': { state: 3, stateOld: 2, cause: 'self' },
   })
   ;`👍` //?
 })
--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,5 +1,12 @@
 import { AtomCache, AtomMeta, Ctx, Fn, Rec } from '@reatom/core'
 
+export interface LogMsg {
+  error: undefined | Error
+  changes: Rec
+  logs: Array<AtomCache>
+  ctx: Ctx
+}
+
 export const getCause = (patch: AtomCache) => {
   let log = `self`
   let cause: typeof patch.cause = patch
@@ -11,29 +18,61 @@ export const getCause = (patch: AtomCache) => {
   return log
 }
 
+export const createLogBatched = (log = console.log) => {
+  let queue: Array<LogMsg> = []
+  const logBatched = (msg: LogMsg) => {
+    Promise.resolve(queue.push(msg)).then((length) => {
+      if (queue.length !== length) return
+
+      console.groupCollapsed(`Reatom ${length} logs`)
+      log(
+        queue.reduce((acc, { changes }, i) => {
+          for (const k in changes) acc[`[${i + 1}] ${k}`] = changes[k]
+          return acc
+        }, {} as Rec),
+        queue,
+      )
+      console.groupEnd()
+      queue = []
+    })
+  }
+
+  return logBatched
+}
+
 export const connectLogger = (
   ctx: Ctx,
   {
-    log = console.log,
+    log = createLogBatched(),
+    skipEmpty = true,
     showCause = true,
-  }: { log?: typeof console.log; showCause?: boolean } = {},
+  }: {
+    log?: Fn<[LogMsg]>
+    skipEmpty?: boolean
+    showCause?: boolean
+  } = {},
 ) => {
   let read: Fn<[AtomMeta], undefined | AtomCache>
   ctx.get((r) => (read = r))
 
   return ctx.subscribe((logs, error) => {
     const counter = new Map<AtomMeta, number>()
+    let empty = true
     const changes = logs.reduce((acc, patch) => {
       const { meta, state } = patch
       const { name } = meta
+      const stateOld = read(meta)?.state
 
       counter.set(meta, (counter.get(meta) ?? 0) + 1)
 
-      if (!name || Object.is(state, read(meta)?.state)) return acc
+      if (!name || Object.is(state, stateOld)) return acc
+
+      empty = false
 
       const message = showCause
         ? {
             state,
+            stateOld,
             get cause() {
               return getCause(patch)
             },
@@ -49,6 +88,8 @@ export const connectLogger = (
 
       return acc
     }, {} as Rec)
+
+    if (skipEmpty && empty) return
 
     log({
       error,
