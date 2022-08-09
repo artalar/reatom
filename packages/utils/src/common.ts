a8c12096d402de7437857ff994e98a03b66c385b
Dan Chu <4urbanoff@gmail.com>
2022-08-09 16:57:45 +0300

    Update shallowEqual util
    Hi!
    It doesn't make sense to compare all properties if somewhere in the middle there are already non-matching ones.

--- packages/utils/src/common.ts
+++ packages/utils/src/common.ts
@@ -7,7 +7,12 @@ export const shallowEqual = (a: any, b: any) => {
   if (!isObject(a) || !isObject(b)) return Object.is(a, b)
   const aKeys = Object.keys(a)
   const bKeys = Object.keys(b)
-  return (
-    aKeys.length === bKeys.length && aKeys.every((k) => Object.is(a[k], b[k]))
-  )
+
+  if (aKeys.length !== bKeys.length) return false
+
+  for (const k of aKeys) {
+    if (!Object.is(a[k], b[k])) return false
+  }
+
+  return true
 }
