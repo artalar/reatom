17f23bbed0da1bc566ca41bcc47ef41fa26ba220
artalar <artalar@ya.ru>
2022-09-15 04:58:45 +0300

    docs: setup vercel

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel()
+});
