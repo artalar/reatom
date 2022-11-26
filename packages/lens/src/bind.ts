4a157c4ab1ff16db713e933d36380bb759dd032c
artalar <artalar@ya.ru>
2022-11-26 16:53:27 +0400

    fix(lens): bind

--- packages/lens/src/bind.ts
+++ packages/lens/src/bind.ts
@@ -5,13 +5,13 @@ export type Binded<T extends Fn> = Fn<CtxParams<T>, ReturnType<T>>
 //   [K in keyof T]: T[K]
 // }
 
-const ctxMap = new WeakMap<Ctx, WeakMap<Fn, Fn>>()
+const ctxMap = new WeakMap<Ctx, WeakMap<Fn, Binded<Fn>>>()
 export const bind = <T extends Fn>(ctx: Ctx, fn: T): Binded<T> => {
   let fnMap = ctxMap.get(ctx)
   if (!fnMap) ctxMap.set(ctx, (fnMap = new WeakMap()))
 
   let bfn = fnMap.get(fn)
-  if (!bfn) fnMap.set(fn, (bfn = fn.bind(ctx)))
+  if (!bfn) fnMap.set(fn, (bfn = fn.bind(null, ctx)))
 
   return bfn as Binded<T>
 }
