4f45842f8b7f5e991ca9ce73b101e4370ca7c3ab
artalar <artalar@ya.ru>
2022-12-09 09:57:37 +0400

    docs: playing with sw

--- docs/public/sw.js
+++ docs/public/sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'cache-and-update-v1'
+const CACHE = 'cache-and-update-v2'
 
 self.addEventListener('install', (event) => {
   // TODO parse `/sitemap-0.xml`
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/'])))
 })
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) => {
   /** @type Request */
   const request = event.request
   const fetchPromise = fetch(request)
@@ -17,12 +17,48 @@ self.addEventListener('fetch', function (event) {
       .catch(() => fetchPromise),
   )
 
-  if (request.method.toLowerCase() === 'get') {
+  if (request.method.toLowerCase() === 'get' && !request.url.endsWith('js')) {
     event.waitUntil(
-      fetchPromise.then((response) => {
-        if (response.status !== 200) return
+      fetchPromise.then(async (response) => {
+        if (
+          response.status !== 200 ||
+          response.headers
+            .get('content-type')
+            ?.includes('application/javascript')
+        ) {
+          return
+        }
         response = response.clone()
-        caches.open(CACHE).then((cache) => cache.put(request, response))
+
+        let staleUpdate = Promise.resolve()
+        if (response.headers.get('content-type')?.includes('text/html')) {
+          staleUpdate = caches
+            .match(request)
+            .then((matching) => matching || null)
+            .then((cache) => cache?.text())
+            .then(
+              (cacheText) =>
+                cacheText &&
+                response
+                  .clone()
+                  .text()
+                  .then((responseText) => {
+                    if (cacheText === responseText) return
+
+                    // const broadcast = new BroadcastChannel('sw')
+                    // broadcast.postMessage({
+                    //   type: 'new-content',
+                    //   text: responseText,
+                    //   url: request.url,
+                    // })
+                  }),
+            )
+            .catch(() => null)
+        }
+
+        return staleUpdate.then(() =>
+          caches.open(CACHE).then((cache) => cache.put(request, response)),
+        )
       }),
     )
   }
