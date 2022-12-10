ca2324f98586d1c6b212fe2f682ae6d6a1fcb875
artalar <artalar@ya.ru>
2022-12-10 17:20:23 +0400

    docs: improve async storytest

--- packages/async/src/index.story.test.ts
+++ packages/async/src/index.story.test.ts
@@ -1,8 +1,9 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
-import { onConnect } from '@reatom/hooks'
 import { createTestCtx } from '@reatom/testing'
-import { isShallowEqual, sleep } from '@reatom/utils'
+
+import { onConnect } from '@reatom/hooks'
+import { isDeepEqual, jsonClone, sleep } from '@reatom/utils'
 
 import { reatomAsync, withDataAtom } from './'
 
@@ -12,22 +13,20 @@ test('optimistic update without extra updates on invalidation', async () => {
   const getData = async () => mock
   const putData = async (id: number, value: number) => {
     await sleep()
-    mock = mock.slice()
+    mock = jsonClone(mock)
     mock.find((item) => item.id === id)!.value = value
   }
   //#endregion
 
   // this is short for test purposes, use ~5000 in real code
-  const INTERVAL = 15
+  const INTERVAL = 5
 
   const fetchData = reatomAsync(getData, 'fetchData').pipe(
-    withDataAtom([], (ctx, payload, state) => {
-      const isEqual = payload.every((item, i) => isShallowEqual(item, state[i]))
-      if (isEqual) return state
-      const result = state.slice()
-      result.splice(0, payload.length, ...payload)
-      return result
-    }),
+    // add `dataAtom` and map the effect payload into it
+    // try to prevent new reference stream if nothing really changed
+    withDataAtom([], (ctx, payload, state) =>
+      isDeepEqual(payload, state) ? state : payload,
+    ),
   )
   const updateData = reatomAsync(
     (ctx, id: number, value: number) => putData(id, value),
@@ -51,16 +50,22 @@ test('optimistic update without extra updates on invalidation', async () => {
   const effectTrack = ctx.subscribeTrack(fetchData.onFulfill)
   const dataTrack = ctx.subscribeTrack(fetchData.dataAtom)
 
+  // every subscription calls passed callback immediately
+  assert.is(effectTrack.calls.length, 1)
   assert.is(dataTrack.calls.length, 1)
   assert.equal(dataTrack.lastInput(), [])
+
+  // `onConnect` calls `fetchData`, wait it and check changes
   await sleep()
   assert.is(dataTrack.calls.length, 2)
   assert.equal(dataTrack.lastInput(), [{ id: 1, value: 1 }])
 
+  // call `updateData` and check changes
   updateData(ctx, 1, 2)
   assert.is(dataTrack.calls.length, 3)
   assert.equal(dataTrack.lastInput(), [{ id: 1, value: 2 }])
 
+  // wait for `fetchData` and check changes
   assert.is(effectTrack.calls.length, 2)
   await sleep(INTERVAL)
   // the effect is called again, but dataAtom is not updated
