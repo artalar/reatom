0f19eab277c00acbbfba02f8a9daa3008d10313a
artalar <artalarut@proton.me>
2022-12-13 09:12:15 +0400

    docs: improve sw types

--- docs/public/sw.js
+++ docs/public/sw.js
@@ -5,74 +5,70 @@ const cacheTypes = ['html', 'css', 'font', 'image']
 // const _log = console.log
 const _log = () => {}
 
-self.addEventListener(
-  'fetch',
-  /** @param {ExtendableEvent} event */
-  (event) => {
-    /** @type Request */
-    const request = event.request
-    const fetchPromise = fetch(request)
-    const id = (Math.random() * 1e10) | 0
-    const log = (...a) => _log(request.url, ...a, id)
+self.addEventListener('fetch', (/** @type {ExtendableEvent} */ event) => {
+  /** @type Request */
+  const request = event.request
+  const fetchPromise = fetch(request)
+  const id = (Math.random() * 1e10) | 0
+  const log = (...a) => _log(request.url, ...a, id)
 
-    event.respondWith(
-      caches
-        .open(CACHE)
-        .then((cache) => cache.match(request))
-        .then((matching) => {
-          if (matching) {
-            log('cache match')
-            return matching
-          }
-          log('cache mismatch')
-          return fetchPromise
-        }),
-    )
-
-    if (request.method.toLowerCase() !== 'get') {
-      log('not get')
-      return
-    }
-    // extension
-    if (!request.url.startsWith('http')) {
-      log('not http')
-      return
-    }
-
-    event.waitUntil(
-      fetchPromise.then(async (response) => {
-        const contentType = response.headers.get('content-type')
-
-        if (
-          !response.ok ||
-          response.status !== 200 ||
-          contentType?.includes('application/javascript')
-        ) {
-          log('not ok')
-          return
+  event.respondWith(
+    caches
+      .open(CACHE)
+      .then((cache) => cache.match(request))
+      .then((matching) => {
+        if (matching) {
+          log('cache match')
+          return matching
         }
-
-        response = response.clone()
-
-        if (contentType.includes('text/html') && !request.url.endsWith('.js')) {
-          await hotUpdate(log, event, request, response)
-        }
-
-        return caches.open(CACHE).then((cache) => {
-          log('cache put')
-          cache.put(request, response)
-        })
+        log('cache mismatch')
+        return fetchPromise
       }),
-    )
-  },
-)
+  )
 
-/**
- * @param {ExtendableEvent} event
- * @param {Request} request
- * @param {Response} response
- */
-const hotUpdate = async (log, event, request, response) => {
+  if (request.method.toLowerCase() !== 'get') {
+    log('not get')
+    return
+  }
+  // extension
+  if (!request.url.startsWith('http')) {
+    log('not http')
+    return
+  }
+
+  event.waitUntil(
+    fetchPromise.then(async (response) => {
+      const contentType = response.headers.get('content-type')
+
+      if (
+        !response.ok ||
+        response.status !== 200 ||
+        contentType?.includes('application/javascript')
+      ) {
+        log('not ok')
+        return
+      }
+
+      response = response.clone()
+
+      if (contentType.includes('text/html') && !request.url.endsWith('.js')) {
+        await hotUpdate(log, event, request, response)
+      }
+
+      return caches.open(CACHE).then((cache) => {
+        log('cache put')
+        cache.put(request, response)
+      })
+    }),
+  )
+})
+
+const hotUpdate = async (
+  log,
+  /** @type {ExtendableEvent} */ event,
+  /** @type {Request} */ request,
+  /** @type {Response} */ response,
+) => {
   log('hotUpdate')
 
   const cacheText = await caches
