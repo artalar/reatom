653f4d023dd0eccfc18b0cc5b5b000b13a22e32f
artalar <artalarut@proton.me>
2023-01-24 08:37:02 +0300

    feat(undo): add reatomUndo

--- packages/undo/src/index.test.ts
+++ packages/undo/src/index.test.ts
@@ -3,9 +3,9 @@ import { createTestCtx } from '@reatom/testing'
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 
-import { withUndo } from './'
+import { reatomUndo, withUndo } from './'
 
-test(`base API`, async () => {
+test('withUndo', async () => {
   const a = atom(0).pipe(withUndo({ length: 5 }))
   const ctx = createTestCtx()
 
@@ -50,6 +50,41 @@ test(`base API`, async () => {
   assert.is(ctx.get(a.isUndoAtom), false)
   assert.is(ctx.get(a.isRedoAtom), true)
   assert.is(ctx.get(a), 0)
+})
+
+test('reatomUndo', () => {
+  const a = atom(0)
+  const b = atom(0)
+  const c = reatomUndo({ a, b })
+  const ctx = createTestCtx()
+  ctx.subscribeTrack(c)
+
+  assert.equal(ctx.get(c), { a: 0, b: 0 })
+
+  a(ctx, 1)
+  a(ctx, 2)
+  b(ctx, 3)
+  a(ctx, 4)
+  assert.equal(ctx.get(c), { a: 4, b: 3 })
+
+  c.undo(ctx)
+  assert.equal(ctx.get(c), { a: 2, b: 3 })
+  assert.is(ctx.get(a), 2)
+  assert.is(ctx.get(b), 3)
+
+  c.redo(ctx)
+  assert.equal(ctx.get(c), { a: 4, b: 3 })
+  assert.is(ctx.get(a), 4)
+  assert.is(ctx.get(b), 3)
+
+  c.jump(ctx, -2)
+  assert.equal(ctx.get(c), { a: 2, b: 0 })
+  assert.is(ctx.get(a), 2)
+  assert.is(ctx.get(b), 0)
+
+  b(ctx, 5)
+  assert.equal(ctx.get(c), { a: 2, b: 5 })
+  assert.is(ctx.get(c.isRedoAtom), false)
 })
 
 test.run()
--- packages/undo/src/index.ts
+++ packages/undo/src/index.ts
@@ -1,78 +1,97 @@
 import {
-  action,
   Action,
+  action,
   atom,
   Atom,
+  AtomCache,
   AtomMut,
+  AtomProto,
   AtomState,
+  Ctx,
   Fn,
   isAction,
   isAtom,
+  Rec,
   throwReatomError,
+  __count,
 } from '@reatom/core'
 import { onUpdate, withInit } from '@reatom/hooks'
+import { isShallowEqual, Plain } from '@reatom/utils'
 
 // TODO
 // https://github.com/omnidan/redux-undo
 
-export interface WithUndo<T> {
+export interface WithUndo<T = any> {
+  clearHistory: Action<[], void>
   historyAtom: Atom<Array<T>>
-  isUndoAtom: Atom<boolean>
+  positionAtom: Atom<number>
   isRedoAtom: Atom<boolean>
-  undo: Action<[], T>
+  isUndoAtom: Atom<boolean>
+  jump: Action<[by: number], T>
   redo: Action<[], T>
+  undo: Action<[], T>
 }
 
+const isJump = (patch: AtomCache, jumpProto: AtomProto): boolean =>
+  patch.cause
+    ? patch.cause.proto === jumpProto || isJump(patch.cause, jumpProto)
+    : false
+
 export const withUndo =
-  <T extends AtomMut & Partial<WithUndo<T>>>({
+  <T extends AtomMut & Partial<WithUndo<AtomState<T>>>>({
     length = 30,
-  }: { length?: number } = {}): Fn<[T], T & WithUndo<T>> =>
+    shouldUpdate = () => true,
+  }: {
+    length?: number
+    shouldUpdate?: Fn<[ctx: Ctx, state: AtomState<T>], boolean>
+  } = {}): Fn<[T], T & WithUndo<AtomState<T>>> =>
   (anAtom) => {
     throwReatomError(
       isAction(anAtom) || !isAtom(anAtom) || typeof anAtom !== 'function',
       'withHistory accepts only mutable atom',
     )
-    if (!anAtom.historyAtom) {
-      const name = `${anAtom.__reatom.name}.historyAtom`
+    if (!anAtom.undo) {
+      const { name } = anAtom.__reatom
+
       const historyAtom = (anAtom.historyAtom = atom<Array<AtomState<T>>>(
         [],
-        name,
+        `${name}.historyAtom`,
       ).pipe(withInit((ctx) => [ctx.get(anAtom)])))
-      const positionAtom = atom(0, `${name}.position`)
+
+      const positionAtom = (anAtom.positionAtom = atom(0, `${name}.position`))
+
       anAtom.isUndoAtom = atom(
         (ctx) => ctx.spy(positionAtom) > 0,
         `${name}.isUndoAtom`,
       )
+
       anAtom.isRedoAtom = atom(
         (ctx) => ctx.spy(positionAtom) < ctx.spy(historyAtom).length - 1,
         `${name}.isRedoAtom`,
       )
 
-      anAtom.undo = action(
-        (ctx) =>
-          anAtom(
-            ctx,
-            ctx.get(historyAtom)[positionAtom(ctx, (s) => Math.max(0, s - 1))],
-          ),
-        `${name}.undo`,
-      )
-      anAtom.redo = action((ctx) => {
+      const jump = (anAtom.jump = action((ctx, by: number) => {
         const history = ctx.get(historyAtom)
-        return anAtom(
-          ctx,
-          history[
-            positionAtom(ctx, (s) => Math.min(history.length - 1, s + 1))
-          ],
-        )
-      }, `${name}.redo`)
+        const position = ctx.get(positionAtom)
+        const to = Math.max(0, Math.min(history.length - 1, position + by))
 
-      const actionsProto = [anAtom.undo.__reatom, anAtom.redo.__reatom]
+        return anAtom(ctx, history[positionAtom(ctx, to)])
+      }, `${name}.jump`))
+
+      anAtom.undo = action((ctx) => jump(ctx, -1), `${name}.undo`)
+
+      anAtom.redo = action((ctx) => jump(ctx, 1), `${name}.redo`)
+
+      anAtom.clearHistory = action((ctx) => {
+        historyAtom(ctx, () => [ctx.get(anAtom)])
+        positionAtom(ctx, 0)
+      }, `${name}.clearHistory`)
 
       onUpdate(anAtom, (ctx, state, patch) => {
         {
-          if (actionsProto.includes(patch.cause?.proto as any)) return
+          if (isJump(patch, jump.__reatom)) return
 
-          historyAtom!(ctx, (history) => {
+          historyAtom(ctx, (history) => {
             if (history[history.length - 1] !== state) {
               history = history.slice(-length + 1)
               if (history.length !== ctx.get(positionAtom) - 1) {
@@ -89,3 +108,31 @@ export const withUndo =
 
     return anAtom as any
   }
+
+type AtomsStates<T> = Plain<{
+  [K in keyof T]: AtomState<T[K]>
+}>
+
+interface UndoAtom<T> extends Atom<T>, WithUndo<T> {}
+
+export const reatomUndo = <T extends Array<AtomMut> | Rec<AtomMut>>(
+  shape: T,
+  name = __count('undoAtom'),
+): UndoAtom<AtomsStates<T>> =>
+  Object.assign(
+    (ctx: Ctx, newShape: AtomsStates<T>) => {
+      for (const [key, anAtom] of Object.entries(shape)) {
+        if (!Object.is(ctx.get(anAtom), newShape[key as keyof T])) {
+          anAtom(ctx, newShape[key as keyof T])
+        }
+      }
+      return newShape
+    },
+    atom((ctx, state = (Array.isArray(shape) ? [] : {}) as AtomsStates<T>) => {
+      const newState = Object.entries(shape).reduce(
+        (acc, [key, anAtom]) => ((acc[key as keyof T] = ctx.spy(anAtom)), acc),
+        (Array.isArray(shape) ? [] : {}) as AtomsStates<T>,
+      )
+      return isShallowEqual(state, newState) ? state : newState
+    }, name),
+  ).pipe(withUndo())
