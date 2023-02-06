0ad77791b2b12a2d1238b20192054cd94e2ef199
artalar <artalarut@proton.me>
2023-02-06 09:40:24 +0300

    feat(history): publish

--- packages/npm-history/src/index.test.ts
+++ packages/npm-history/src/index.test.ts
@@ -1,10 +1,90 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
+/* EXPERIMENTAL */
 
-import {} from './'
+import { Atom, Action, atom, action, Rec } from '@reatom/core'
+import { parsePath, Path } from 'history'
+import { historyAtom } from '.'
 
-test(`base API`, async () => {
-  assert.ok(false, `You forgot test you code`)
-})
+export interface RouteAtom extends Atom<boolean>, Partial<Path> {
+  go: Action<[], void>
+}
 
-test.run()
+interface RoutesScheme {
+  [key: string]: RoutesScheme | string
+}
+
+type Routes<T extends RoutesScheme | string> = T extends ''
+  ? RouteAtom
+  : T extends `${string}/${string}`
+  ? never
+  : RouteAtom & {
+      routes: {
+        [K in keyof T as K extends `${infer K}/${string}`
+          ? K
+          : K]: K extends `${string}/:${infer Param}`
+          ? // @ts-ignore
+            Routes<Record<Param, T[K]>> & { param: Atom<string> }
+          : // @ts-ignore
+            Routes<T[K]>
+      }
+    }
+
+export const unstable_reatomRoutes = <T extends RoutesScheme>(
+  base: string,
+  routes: T,
+): Routes<T> => {
+  const path = parsePath(base)
+  const routeAtom = atom((ctx) =>
+    // TODO tests
+    ctx.spy(historyAtom.location).pathname.startsWith(path.pathname || ''),
+  )
+
+  const res = Object.assign(routeAtom, path, {
+    go: action((ctx) => historyAtom.push(ctx, base)),
+    routes: Object.keys(routes).reduce((acc, key) => {
+      const nextIdx = key.indexOf('/')
+      const subRoutes = (
+        typeof routes[key] === 'object' ? routes[key] : {}
+      ) as Rec
+      let isDynamic = false
+
+      if ((isDynamic = key.startsWith(':'))) {
+        key = key.slice(1)
+      }
+
+      if (nextIdx !== -1) {
+        subRoutes[key.slice(nextIdx + 1)] = ''
+        key = key.slice(0, nextIdx)
+      }
+
+      const routeAtom = (acc[key] = unstable_reatomRoutes(
+        `${base === '/' ? '' : base}/${key}`,
+        subRoutes,
+      ))
+
+      if (isDynamic) {
+        console.log({ isDynamic, base })
+        // @ts-expect-error
+        routeAtom.param = atom((ctx) => {
+          console.log('re', ctx.spy(routeAtom))
+          if (!ctx.spy(routeAtom)) {
+            return ''
+          }
+
+          const { pathname } = ctx.spy(historyAtom.location)
+
+          const paramPath = pathname.slice(0, base.length)
+
+          const nextIdx = paramPath.indexOf('/')
+
+          console.log({ pathname, paramPath, nextIdx })
+
+          return nextIdx === -1 ? paramPath : paramPath.slice(0, nextIdx)
+        })
+      }
+
+      return acc
+    }, {} as any),
+  })
+
+  return res as any
+}
--- packages/npm-history/src/index.ts
+++ packages/npm-history/src/index.ts
@@ -1,191 +1,94 @@
-import { History, Location, To, Blocker, createMemoryHistory } from 'history'
+import { History, Location, To, Blocker } from 'history'
 
-import { action, atom, Atom, Action, throwReatomError } from '@reatom/core'
+import {
+  action,
+  atom,
+  Atom,
+  AtomMut,
+  Action,
+  throwReatomError,
+} from '@reatom/core'
 import { onUpdate } from '@reatom/hooks'
 import { isShallowEqual } from '@reatom/utils'
 
-export interface LocationAtom extends Atom<Location> {
+export interface HistoryAtom extends AtomMut<History> {
+  back: Action<[]>
+  block: Action<[blocker: Blocker], () => void>
+  forward: Action<[]>
+  go: Action<[delta: number]>
+  location: Atom<Location>
   push: Action<[to: To, state?: any]>
   replace: Action<[to: To, state?: any]>
-  go: Action<[delta: number]>
-  back: Action<[]>
-  forward: Action<[]>
-  block: Action<[blocker: Blocker], () => void>
 }
 
-const historyAtom = atom<null | History>(null, 'historyAtom')
-
-const _locationAtom = atom((ctx): Location => {
-  const history = ctx.spy(historyAtom)
-
-  throwReatomError(!history, 'history not initialized')
-
-  let { location } = history!
-
-  const locationUpdate = ctx.spy(updateLocation)
-  if (locationUpdate.length) {
-    location = locationUpdate[locationUpdate.length - 1]!.payload
-  }
-
-  return Object.assign({}, location)
-}, 'locationAtom')
-
-const updateLocation = action<Location>('updateLocation')
-
-const push = action((ctx, to: To, state?: any) => {
-  ctx.schedule(() => ctx.get(historyAtom)!.push(to, state))
-  const history = createMemoryHistory({
-    initialEntries: [Object.assign({}, ctx.get(_locationAtom))],
-  })
-  history.push(to, state)
-  updateLocation(ctx, history.location)
-}, 'historyAtom.push')
-
-const replace = action((ctx, to: To, state?: any) => {
-  ctx.schedule(() => ctx.get(historyAtom)!.replace(to, state))
-  const history = createMemoryHistory({
-    initialEntries: [Object.assign({}, ctx.get(_locationAtom))],
-  })
-  history.replace(to, state)
-  updateLocation(ctx, history.location)
-}, 'historyAtom.replace')
-
-const go = action((ctx, delta: number) => {
-  ctx.schedule(() => ctx.get(historyAtom)!.go(delta))
-  const history = createMemoryHistory({
-    initialEntries: [Object.assign({}, ctx.get(_locationAtom))],
-  })
-  history.go(delta)
-  updateLocation(ctx, history.location)
-}, 'historyAtom.go')
-
-const back = action((ctx) => {
-  ctx.schedule(() => ctx.get(historyAtom)!.back())
-  const history = createMemoryHistory({
-    initialEntries: [Object.assign({}, ctx.get(_locationAtom))],
-  })
-  history.back()
-  updateLocation(ctx, history.location)
-}, 'historyAtom.back')
-
-const forward = action((ctx) => {
-  ctx.schedule(() => ctx.get(historyAtom)!.forward())
-  const history = createMemoryHistory({
-    initialEntries: [Object.assign({}, ctx.get(_locationAtom))],
-  })
-  history.forward()
-  updateLocation(ctx, history.location)
-}, 'historyAtom.forward')
-
-const block = action((ctx, blocker: Blocker) => {
-  ctx.schedule(() => ctx.get(historyAtom)!.block(blocker))
-  const history = createMemoryHistory({
-    initialEntries: [Object.assign({}, ctx.get(_locationAtom))],
-  })
-  history.block(blocker)
-  updateLocation(ctx, history.location)
-}, 'historyAtom.block')
-
-const locationAtom: LocationAtom = Object.assign(_locationAtom, {
-  push,
-  replace,
-  go,
-  back,
-  forward,
-  block,
-})
-
+// @ts-expect-error
+export const historyAtom: HistoryAtom = atom(null, 'historyAtom')
+// @ts-expect-error
+historyAtom.__reatom.computer = (ctx, history?: History) => (
+  throwReatomError(!history, 'history not initialized'), history
+)
 onUpdate(historyAtom, (ctx, history) => {
-  // FIXME clone
-  internalHistoryAtom(ctx, [history])
-})
-onUpdate(historyAtom, (ctx, history) => {
-  throwReatomError(!history, 'history not initialized')
-
-  ctx.schedule(() =>
-    history!.listen(({ location, action }) => {
-      if (!isShallowEqual(ctx.get(_locationAtom), location)) {
-        updateLocation(ctx, location)
-      }
-    }),
+  history.listen(
+    ({ location, action }) =>
+      isShallowEqual(ctx.get(locationAtom), location) ||
+      // @ts-expect-error
+      locationAtom(ctx, history.location),
   )
 })
 
-export { historyAtom, locationAtom }
+// @ts-expect-error
+const locationAtom: HistoryAtom['location'] = atom(null, 'historyAtom.location')
+locationAtom.__reatom.computer = (ctx) =>
+  Object.assign({}, ctx.spy(historyAtom).location)
 
-// let pathPieces = parsePath('/the/path?the=query#the-hash')
-// // pathPieces = {
-// //   pathname: '/the/path',
-// //   search: '?the=query',
-// //   hash: '#the-hash'
-// // }
+const push: HistoryAtom['push'] = action((ctx, to: To, state?: any) => {
+  const history = ctx.get(historyAtom)
+  history.push(to, state)
+  // @ts-expect-error
+  locationAtom(ctx, history.location)
+}, 'historyAtom.push')
 
-// export interface LocationPathAtom extends Atom<string> {
-//   change: Action<[string]>
-// }
+const replace: HistoryAtom['replace'] = action((ctx, to: To, state?: any) => {
+  const history = ctx.get(historyAtom)
+  history.replace(to, state)
+  // @ts-expect-error
+  locationAtom(ctx, history.location)
+}, 'historyAtom.replace')
 
-// export const reatomLocation = ({
-//   scheduler = globalThis.requestAnimationFrame ??
-//     ((resolve) => setTimeout(resolve, 16)),
-// }: { scheduler?: Fn<[Fn]> } = {}): LocationPathAtom => {
-//   const locationPathAtom = atom('/', 'locationPathAtom').pipe(
-//     withInit(() => globalThis.location.pathname),
-//   )
-//   onConnect(locationPathAtom, async (ctx) => {
-//     do {
-//       await new Promise(scheduler)
-//       if (location.pathname !== ctx.get(locationPathAtom)) {
-//         locationPathAtom(ctx, location.pathname)
-//       }
-//     } while (ctx.isConnected())
-//   })
-//   const change = action((ctx, path: string) => {
-//     locationPathAtom(ctx, path)
-//     ctx.schedule(() => {
-//       // location.pathname = path
-//       history.pushState({}, document.title, path)
-//     })
-//   }, 'locationPathAtom.change')
+const go: HistoryAtom['go'] = action((ctx, delta: number) => {
+  const history = ctx.get(historyAtom)
+  history.go(delta)
+  // @ts-expect-error
+  locationAtom(ctx, history.location)
+}, 'historyAtom.go')
 
-//   return Object.assign(locationPathAtom, { change })
-// }
+const back: HistoryAtom['back'] = action((ctx) => {
+  const history = ctx.get(historyAtom)
+  history.back()
+  // @ts-expect-error
+  locationAtom(ctx, history.location)
+}, 'historyAtom.back')
 
-// export interface IsRouteAtom<Route extends string | RegExp>
-//   extends Atom<boolean> {
-//   path: Route
-//   go: Route extends string ? Action<[], void> : never
-// }
+const forward: HistoryAtom['forward'] = action((ctx) => {
+  const history = ctx.get(historyAtom)
+  history.forward()
+  // @ts-expect-error
+  locationAtom(ctx, history.location)
+}, 'historyAtom.forward')
 
-// export const reatomRoutes = <T extends Rec<string | RegExp>>(
-//   locationPathAtom: LocationPathAtom,
-//   routes: T,
-// ): {
-//   [K in keyof T as `${K}Atom`]: IsRouteAtom<T[K]>
-// } => {
-//   const res: Rec = {}
-//   for (const k in routes) {
-//     const name = `${k}Atom`
-//     const route = routes[k]
-//     const routeAtom = atom((ctx) => {
-//       const path = ctx.spy(locationPathAtom)
-//       const match =
-//         route instanceof RegExp ? route.test(path) : path.startsWith(route)
-//       return match
-//     }, name)
-//     res[name] = routeAtom
-//     // @ts-expect-error
-//     routeAtom.go = action((ctx) => {
-//       throwReatomError(route instanceof RegExp, 'string route expected')
-//       locationPathAtom.change(ctx, route as string)
-//     }, `${name}.go`)
-//   }
+const block: HistoryAtom['block'] = action((ctx, blocker: Blocker) => {
+  const history = ctx.get(historyAtom)
+  const unblock = history.block(blocker)
+  // @ts-expect-error
+  locationAtom(ctx, history.location)
 
-//   return res as any
-// }
+  return unblock
+}, 'historyAtom.block')
 
-// export const locationPathAtom = reatomLocation()
-// export const routes = reatomRoutes(locationPathAtom, {
-//   home: '/main',
-//   login: '/auth/login',
-//   register: '/auth/register',
-// })
+historyAtom.back = back
+historyAtom.block = block
+historyAtom.forward = forward
+historyAtom.go = go
+historyAtom.location = locationAtom
+historyAtom.push = push
+historyAtom.replace = replace
