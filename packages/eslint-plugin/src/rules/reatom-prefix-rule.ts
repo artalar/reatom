dbbe3ef6460a9fda84bb71c06e16b7e85df790b1
Anton Krylov <adkrylov@avito.ru>
2023-03-02 05:48:50 +0600

    fix: update package-lock.json

--- packages/eslint-plugin/src/rules/reatom-prefix-rule.ts
+++ packages/eslint-plugin/src/rules/reatom-prefix-rule.ts
@@ -15,20 +15,20 @@ export const reatomPrefixRule: Rule.RuleModule = {
                 if (d.init.callee.type !== 'Identifier') return;
                 if (!d.init.callee.name.startsWith('reatom')) return;
                 if (d.id.type !== 'Identifier') return;
-                
+
                 if (d.init.arguments[0]?.type === 'ArrowFunctionExpression') {
                     if (d.init.arguments.length === 1) {
-                        context.report({ 
+                        context.report({
                             message: `some reatom* name is not defined`,
                             node: d,
-                            fix: fixer => fixer.insertTextAfterRange(d.init.arguments[0].range, `, "${d.id.name}"`)
+                            fix: fixer => fixer.insertTextAfter(d.init.arguments[0], `, "${d.id.name}"`)
                         })
                     }
 
                     if (d.init.arguments.length === 2) {
                         if (d.init.arguments[1]?.type === 'Literal' && d.init.arguments[1].value !== d.id.name) {
                             context.report({
-                                message: `some reatom* name is defined bad`, 
+                                message: `some reatom* name is defined bad`,
                                 node: d,
                                 fix: fixer => fixer.replaceText(d.init.arguments[1], `"${d.id.name}"`)
                             })
@@ -38,24 +38,24 @@ export const reatomPrefixRule: Rule.RuleModule = {
                             if (d.init.arguments[1].properties.some(value => value.type === 'Property' && value.key.type === 'Identifier')) {
                                 if (!d.init.arguments[1].properties.some(value => value.key.name === 'name')) {
                                     context.report({
-                                        message: `some reatom* name is not defined`, 
+                                        message: `some reatom* name is not defined`,
                                         node: d,
                                         fix: fixer => fixer.insertTextBefore(d.init.arguments[1].properties[0], `name: "${d.id.name}", `)
                                     })
                                 }
 
                                 const badProperty = d.init.arguments[1].properties.find(
-                                    value => 
-                                        value.type === 'Property' && 
-                                        value.key.type === 'Identifier' && 
-                                        value.key.name === 'name' && 
+                                    value =>
+                                        value.type === 'Property' &&
+                                        value.key.type === 'Identifier' &&
+                                        value.key.name === 'name' &&
                                         value.value.type === 'Literal' &&
                                         value.value.value !== d.id.name
                                 )
 
                                 if (badProperty) {
                                     context.report({
-                                        message: `some reatom* name is defined bad`, 
+                                        message: `some reatom* name is defined bad`,
                                         node: d,
                                         fix: fixer => fixer.replaceText(badProperty.value, `"${d.id.name}"`)
                                     })
