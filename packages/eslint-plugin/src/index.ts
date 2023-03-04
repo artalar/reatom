085cae883baee3d472fe606e0b51b02012fcd2a0
Anton Krylov <adkrylov@avito.ru>
2023-03-05 02:20:11 +0600

    fix: proper names

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -3,7 +3,17 @@ import { atomRule } from "./rules/atom-rule";
 import { reatomPrefixRule } from "./rules/reatom-prefix-rule";
 
 export const rules = {
-    atomRule,
-    actionRule,
-    reatomPrefixRule
+    'atom-rule': atomRule,
+    'action-rule': actionRule,
+    'reatom-prefux-rule': reatomPrefixRule
+}
+
+export const configs = {
+    'recommended': {
+        rules: {
+            'reatom/atom-rule': 'error',
+            'reatom/action-rule': 'error',
+            'reatom/reatom-prefux-rule': 'error'
+        }
+    }
 }
