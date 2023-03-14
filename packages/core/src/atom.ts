bc702441b11ab75aa4af0ff45de690218a568bae
BANO.notIT <BANOnotIT@users.noreply.github.com>
2023-03-14 17:04:10 +0300

    fix(core): use falsy condition in throwError (#501)

--- packages/core/src/atom.ts
+++ packages/core/src/atom.ts
@@ -161,10 +161,13 @@ export interface Unsubscribe {
 
 //#region DOMAIN UTILS
 
+// We don't have type literal for NaN but other values are presented here
+// https://stackoverflow.com/a/51390763
+type Falsy = false | 0 | "" | null | undefined 
 // Can't be an arrow function due to 
 //    https://github.com/microsoft/TypeScript/issues/34523
 /** Throws `Reatom error: ${message}` */
-export function throwReatomError(condition: any, message: string): asserts condition {
+export function throwReatomError(condition: any, message: string): asserts condition is Falsy {
   if (condition)
     throw new Error(`Reatom error: ${message}`)
 }
