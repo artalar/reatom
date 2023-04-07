14d79d73a62506096c1e3a1b3a969b46c77f85a8
lordszn3 <robertkuzhin@gmail.com>
2023-04-07 23:27:04 +0500

    fix: fix the detection of uncorrect name when importing through aliases
    * Format the code and indents for standard
    
    * Implement base tests case to check import aliases

--- packages/eslint-plugin/src/rules/atom-rule.ts
+++ packages/eslint-plugin/src/rules/atom-rule.ts
@@ -1,66 +1,99 @@
-import { Rule} from "eslint";
-import { CallExpression, Identifier, Literal, VariableDeclarator, Node } from 'estree';
-import { isIdentifier, isLiteral } from "../lib";
+import { Rule } from 'eslint'
+import {
+  CallExpression,
+  Identifier,
+  Literal,
+  VariableDeclarator,
+  Node,
+} from 'estree'
+import { extractImportDeclaration, isIdentifier, isLiteral } from '../lib'
 
-type AtomCallExpression = CallExpression & { callee: Identifier, arguments: [Literal] | [Literal, Literal] }
-type AtomVariableDeclarator = VariableDeclarator & { id: Identifier, init: AtomCallExpression }
+type AtomCallExpression = CallExpression & {
+  callee: Identifier
+  arguments: [Literal] | [Literal, Literal]
+}
+type AtomVariableDeclarator = VariableDeclarator & {
+  id: Identifier
+  init: AtomCallExpression
+}
 
-const noname = (atomName: string) => `atom "${atomName}" should has a name inside atom() call`;
-const invalidName = (atomName: string) => `atom "${atomName}" should be named as it's variable name, rename it to "${atomName}"`;
+const noname = (atomName: string) =>
+  `atom "${atomName}" should has a name inside atom() call`
+const invalidName = (atomName: string) =>
+  `atom "${atomName}" should be named as it's variable name, rename it to "${atomName}"`
 
 export const atomRule: Rule.RuleModule = {
-    meta: {
-        type: 'suggestion',
-        docs: {
-            description: "Add name for every atom call"
-        },
-        fixable: 'code'
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'Add name for every atom call',
     },
-    create: function (context: Rule.RuleContext): Rule.RuleListener {
-        let importedFromReatom = false;
+    fixable: 'code',
+  },
+  create: function (context: Rule.RuleContext): Rule.RuleListener {
+    const importedFromReatom = new Map<string, string>()
 
-        return {
-            ImportSpecifier(node) {
-                const imported = node.imported.name;
-                // @ts-ignore
-                const from = node.parent.source.value;
-                if (from.startsWith('@reatom') && imported === 'atom') {
-                    importedFromReatom = true;
-                }
-            },
-            VariableDeclarator: d => {
-                if (!isAtomVariableDeclarator(d) || !importedFromReatom) return;
+    return {
+      ImportDeclaration(node) {
+        extractImportDeclaration({
+          from: node,
+          importsAlias: importedFromReatom,
+          packageName: '@reatom',
+        })
+      },
+      VariableDeclarator: (d) => {
+        if (!isAtomVariableDeclarator(d, importedFromReatom)) return
 
-                if (d.init.arguments.length === 1) {
-                    reportUndefinedAtomName(context, d);
-                } else if (isLiteral(d.init.arguments[1]) && d.init.arguments[1].value !== d.id.name) {
-                    reportBadAtomName(context, d);
-                }
-            }
-        };
+        if (d.init.arguments.length === 1) {
+          reportUndefinedAtomName(context, d)
+        } else if (
+          isLiteral(d.init.arguments[1]) &&
+          d.init.arguments[1].value !== d.id.name
+        ) {
+          reportBadAtomName(context, d)
+        }
+      },
     }
+  },
 }
 
-function isAtomCallExpression(node?: Node | null): node is AtomCallExpression {
-    return node?.type === 'CallExpression' && node.callee?.type === 'Identifier' && node.callee.name === 'atom';
+function isAtomCallExpression(
+  node: Node | null | undefined,
+  imported: Map<string, string>,
+): node is AtomCallExpression {
+  return (
+    node?.type === 'CallExpression' &&
+    node.callee?.type === 'Identifier' &&
+    imported.get('atom') === node.callee.name
+  )
 }
 
-function isAtomVariableDeclarator(node: VariableDeclarator): node is AtomVariableDeclarator {
-    return isAtomCallExpression(node.init) && isIdentifier(node.id);
+function isAtomVariableDeclarator(
+  node: VariableDeclarator,
+  importedFromReatom: Map<string, string>,
+): node is AtomVariableDeclarator {
+  return isAtomCallExpression(node.init, importedFromReatom) && isIdentifier(node.id)
 }
 
-function reportUndefinedAtomName(context: Rule.RuleContext, d: AtomVariableDeclarator) {
-    context.report({
-        message: noname(d.id.name),
-        node: d,
-        fix: fixer => fixer.insertTextAfter(d.init.arguments[0], `, "${d.id.name}"`)
-    });
+function reportUndefinedAtomName(
+  context: Rule.RuleContext,
+  d: AtomVariableDeclarator,
+) {
+  context.report({
+    message: noname(d.id.name),
+    node: d,
+    fix: (fixer) =>
+      fixer.insertTextAfter(d.init.arguments[0], `, "${d.id.name}"`),
+  })
 }
 
-function reportBadAtomName(context: Rule.RuleContext, d: AtomVariableDeclarator) {
-    context.report({
-        message: invalidName(d.id.name),
-        node: d,
-        fix: fixer => fixer.replaceText(d.init.arguments[1], `"${d.id.name}"`)
-    });
+function reportBadAtomName(
+  context: Rule.RuleContext,
+  d: AtomVariableDeclarator,
+) {
+  context.report({
+    message: invalidName(d.id.name),
+    node: d,
+    fix: (fixer) => fixer.replaceText(d.init.arguments[1], `"${d.id.name}"`),
+  })
 }
