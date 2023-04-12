230a9c9f920273d16eae67344acc309ad583f068
artalar <artalarut@proton.me>
2023-04-12 09:49:35 +0300

    fix(lens): use onUpdate

--- packages/lens/src/onLensUpdate.ts
+++ packages/lens/src/onLensUpdate.ts
@@ -1,43 +1,11 @@
-import {
-  Action,
-  Atom,
-  AtomCache,
-  AtomMut,
-  AtomState,
-  Ctx,
-  Fn,
-  Unsubscribe,
-} from '@reatom/core'
+import { Atom } from '@reatom/core'
 import { __thenReatomed } from '@reatom/effects'
 import { onUpdate } from '@reatom/hooks'
-import { assign, noop } from '@reatom/utils'
+import { assign } from '@reatom/utils'
 import { type LensAtom } from './'
 
-export const onLensUpdate: {
-  <Params extends any[], Payload>(
-    anAction: Action<Params, Payload>,
-    cb?: Fn<
-      [
-        Ctx,
-        Payload,
-        AtomCache<AtomState<Action<Params, Payload>>> & { params: Params },
-      ]
-    >,
-  ): Unsubscribe
-  <T>(
-    anAtom: AtomMut<T> | LensAtom<T>,
-    cb?: Fn<[Ctx, T, AtomCache<T>]>,
-  ): Unsubscribe
-} = (anAtom: Atom, fn = noop) =>
-  ((anAtom as LensAtom).deps ?? []).reduce((acc, dep) => {
-    const un = onLensUpdate(dep as LensAtom, (ctx) => {
-      ctx.get(anAtom)
-    })
-    return () => {
-      un()
-      acc()
-    }
-  }, onUpdate(anAtom, fn) as Unsubscribe)
+/** @deprecated use regular `onUpdate` instead */
+export const onLensUpdate: typeof onUpdate = onUpdate
 
 export const toLens =
   <T extends Atom>(deps: Array<Atom>) =>
