b3cc9b8f706f57c1c123fed395933a6221b1038b
BANO.notIT <bano.notit@yandex.ru>
2023-05-03 02:00:15 +0300

    feat(all-settled): add deprecation tsdoc

--- packages/all-settled/src/index.ts
+++ packages/all-settled/src/index.ts
@@ -2,4 +2,7 @@ import { takeNested } from '@reatom/effects'
 
 console.warn("This package is outdated and will be removed soon, `all-settled` is `takeNested` now and part of the [effects](https://www.reatom.dev/packages/effects) package")
 
+/**
+ * @deprecated use {@link @reatom/effects#takeNested}
+ */
 export const allSettled = takeNested
