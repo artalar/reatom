16902b50f24910a9af8d4a2355c65a4bf97e4d0b
Alexandr Dubinin <akiyamka@gmail.com>
2023-05-31 11:54:24 +0200

    fix(core): add missing export

--- packages/core/src/atom.ts
+++ packages/core/src/atom.ts
@@ -607,9 +607,9 @@ let i = 0
  */
 export let __count = (name: string) => `${name}#${++i}`
 
-function atom<T>(initState: (ctx: CtxSpy) => T, name?: string): Atom<T>
-function atom<T>(initState: T, name?: string): AtomMut<T>
-function atom<T>(
+export function atom<T>(initState: (ctx: CtxSpy) => T, name?: string): Atom<T>
+export function atom<T>(initState: T, name?: string): AtomMut<T>
+export function atom<T>(
   initState: T | ((ctx: CtxSpy) => T),
   name = __count('_atom'),
 ): Atom<T> | AtomMut<T> {
