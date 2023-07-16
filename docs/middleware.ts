1f4e083f5ef8b94fb3940e2acd18d1ba6f2504fd
artalar <artalarut@proton.me>
2023-07-16 06:58:31 +0300

    docs: vercel middleware for analytics

---
+++ docs/middleware.ts
@@ -0,0 +1,30 @@
+import { next, geolocation } from '@vercel/edge'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://qratowognuwxoywxegcs.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY as string
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: { persistSession: false },
+})
+
+const log = async (req: Request) => {
+  try {
+    let requestData = {
+      created_at: new Date().toISOString(),
+      path: req.url,
+      geo: geolocation(req),
+    }
+
+    await supabase.from('logs').insert(requestData)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export default async function middleware(req: Request): Promise<Response> {
+  if (req.headers.get('accept')?.includes('text/html')) {
+    await log(req)
+  }
+
+  return next()
+}
