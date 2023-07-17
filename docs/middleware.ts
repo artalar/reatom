e8b08ea3325d3a12e87c822c5c06b1a9918c3ddf
artalar <artalarut@proton.me>
2023-07-17 07:10:55 +0300

    docs: clarify middleware and add counter to headers

--- docs/middleware.ts
+++ docs/middleware.ts
@@ -1,31 +1,25 @@
 import { next } from '@vercel/edge'
 import { kv } from '@vercel/kv'
 
-const IS_DEV = process.env.VERCEL_ENV === 'development'
-
 export default async function middleware(req: Request): Promise<Response> {
-  const url = new URL(req.url)
-  const shouldLog =
-    req.headers.get('accept')?.includes('text/html') &&
-    (url.origin.endsWith('reatom.dev') || IS_DEV)
+  const headers: ResponseInit['headers'] = {}
 
-  if (shouldLog) {
+  if (
+    req.headers.get('accept')?.includes('text/html') &&
+    new URL(req.url).origin.endsWith('reatom.dev')
+  ) {
     try {
       const today = new Date()
       today.setUTCMilliseconds(0)
       today.setUTCSeconds(0)
       today.setUTCMinutes(0)
       today.setUTCHours(0)
-      const key = today.toISOString()
 
-      console.log('key', key)
-      console.time('kv.incr')
-      await kv.incr(key)
-      console.timeEnd('kv.incr')
+      headers['x-visit-count'] = String(await kv.incr(today.toISOString()))
     } catch (error) {
       console.error(error)
     }
   }
 
-  return next()
+  return next({ headers })
 }
