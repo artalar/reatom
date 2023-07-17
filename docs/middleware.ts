3218c215af7f06b7bcbb7ce555c5d0e97397b095
artalar <artalarut@proton.me>
2023-07-17 07:35:42 +0300

    docs: small refactor

--- docs/middleware.ts
+++ docs/middleware.ts
@@ -3,20 +3,15 @@ import { kv } from '@vercel/kv'
 
 export default async function middleware(req: Request): Promise<Response> {
   const headers: ResponseInit['headers'] = {}
-
   if (
     req.headers.get('accept')?.includes('text/html') &&
     new URL(req.url).origin.endsWith('reatom.dev')
   ) {
-    try {
-      const today = new Date()
-      today.setUTCHours(0, 0, 0, 0)
+    const today = new Date()
+    today.setUTCHours(0, 0, 0, 0)
 
-      headers['x-visit-count'] = String(await kv.incr(today.toISOString()))
-    } catch (error) {
-      console.error(error)
-    }
+    const count = await kv.incr(today.toISOString()).catch(() => 0)
+    headers['x-visit-count'] = count.toString()
   }
-
   return next({ headers })
 }
