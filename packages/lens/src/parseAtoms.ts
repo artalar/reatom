d5124230162019fdf025fbd003aa7bbf793f1f4f
krulod <chaurka.noyan@gmail.com>
2023-10-15 01:31:33 +0300

    revert(lens): dont unwrap records in parseAtoms
    This reverts commit 0ab08dd7277de849ce08a248ef4cb76a158b7c61.

--- packages/lens/src/parseAtoms.ts
+++ packages/lens/src/parseAtoms.ts
@@ -23,7 +23,7 @@ export const parseAtoms = <Value>(
 
   if (isRec(value)) {
     const res = {} as Rec
-    for (const k in value) res[k] = parseAtoms(ctx, value[k])
+    for (const k in value) res[k] = value[k]
     return res as any
   }
 
