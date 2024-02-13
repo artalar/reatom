5fee99d8caeb987ea5c15ee6fd119cd309cd7e41
artalar <artalarut@proton.me>
2024-02-13 05:19:43 +0300

    docs: vue-search refactor

--- examples/vue-search/src/model.ts
+++ examples/vue-search/src/model.ts
@@ -4,33 +4,12 @@ import {
   atom,
   reatomResource,
   sleep,
-  withAbort,
   withCache,
   withDataAtom,
   withErrorAtom,
   withRetry,
 } from '@reatom/framework'
-
-export interface Issue {
-  repository_url: string
-  labels_url: string
-  comments_url: string
-  events_url: string
-  html_url: string
-  number: number
-  title: string
-  user: any
-  labels: object[]
-  state: string
-  locked: string
-  assignees: object[]
-  milestone: { url: string }
-  comments: 2
-  author_association: string
-  body: string
-  created_at: string
-  closed_by: object
-}
+import { searchIssues } from './api'
 
 export const searchAtom = atom('', 'searchAtom')
 
@@ -56,18 +35,3 @@ export const issues = reatomResource(async (ctx) => {
     },
   }),
 )
-
-const searchIssues = async (config: {
-  query: string
-  controller: AbortController
-}) => {
-  const response = await fetch(
-    `https://api.github.com/search/issues?q=${config.query}&page=1&per_page=10`,
-    {signal: config.controller.signal},
-  )
-  if (response.status !== 200) {
-    const error = new Error()
-    throw Object.assign(error, await response.json())
-  }
-  return (await response.json()) as { items: Issue[] }
-}
