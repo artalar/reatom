bcd4837971688b1b995d338127b5fa4735597760
krulod <chaurka.noyan@gmail.com>
2024-03-05 16:15:09 +0200

    test(lens): parseAtoms cache

--- packages/lens/src/parseAtoms.test.ts
+++ packages/lens/src/parseAtoms.test.ts
@@ -151,4 +151,22 @@ test('should parse deep structures', () => {
   ;`👍` //?
 })
 
+test.only('cached references', () => {
+  const a = atom([1])
+  const aRec = { a }
+  const b = atom([2])
+  const bRec = { b }
+  const c = atom({ a: aRec.a, b: bRec.b })
+  const ctx = createTestCtx()
+
+  const snapshot = parseAtoms(ctx, c)
+  console.log({ snapshot })
+  assert.is(snapshot, parseAtoms(ctx, c))
+
+  a(ctx, [10])
+  assert.is.not(snapshot, parseAtoms(ctx, c))
+  assert.is.not(snapshot.a, parseAtoms(ctx, c).a)
+  assert.is(snapshot.b, parseAtoms(ctx, c).b)
+})
+
 test.run()
