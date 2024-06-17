dae8710bc0971a3a3ccbbc0d0a5d493d930c8882
artalar <artalar@ya.ru>
2024-06-17 15:23:57 +0300

    chore: add logs to publish script

--- tools/publish.ts
+++ tools/publish.ts
@@ -5,27 +5,15 @@ import fs from 'fs/promises'
 import { createInterface } from 'readline'
 
 const updateFramework = async () => {
-  const packageJson: { version: string; dependencies: Record<string, string> } =
-    JSON.parse(
-      await fs.readFile(
-        path.join(process.cwd(), 'packages', 'framework', 'package.json'),
-        'utf8',
-      ),
-    )
+  const packageJson: { version: string; dependencies: Record<string, string> } = JSON.parse(
+    await fs.readFile(path.join(process.cwd(), 'packages', 'framework', 'package.json'), 'utf8'),
+  )
   const { dependencies } = packageJson
   let isChanged = false
 
   for (const name in dependencies) {
     const { version }: { version: string } = JSON.parse(
-      await fs.readFile(
-        path.join(
-          process.cwd(),
-          'packages',
-          name.replace('@reatom/', ''),
-          'package.json',
-        ),
-        'utf8',
-      ),
+      await fs.readFile(path.join(process.cwd(), 'packages', name.replace('@reatom/', ''), 'package.json'), 'utf8'),
     )
 
     isChanged ||= dependencies[name] !== `^${version}`
@@ -38,9 +26,7 @@ const updateFramework = async () => {
   const [major, minor, patch] = packageJson.version.split('.')
   packageJson.version = [major, minor, Number(patch) + 1].join('.')
 
-  const prettierConfig = await prettier.resolveConfig(
-    path.join(process.cwd(), '.prettierrc'),
-  )
+  const prettierConfig = await prettier.resolveConfig(path.join(process.cwd(), '.prettierrc'))
   await fs.writeFile(
     path.join(process.cwd(), 'packages', 'framework', 'package.json'),
     await prettier.format(JSON.stringify(packageJson), {
@@ -76,21 +62,18 @@ const main = async () => {
 
     let npmVersion = '-1'
     try {
-      npmVersion = (
-        await $`npm view @reatom/${packageName}@${tag} version`
-      ).stdout.trim()
+      npmVersion = (await $`npm view @reatom/${packageName}@${tag} version`).stdout.trim()
     } catch (error) {
       console.warn(`"${packageName}" is not published yet`)
 
-      let answer = await new Promise((r) =>
-        rl.question('continue?(y/n = y)', r),
-      )
+      let answer = await new Promise((r) => rl.question('continue?(y/n = y)', r))
       answer ||= 'y'
       if (answer !== 'y') continue
     }
 
-    if (packageJSON.version !== npmVersion) {
-
+    if (packageJSON.version === npmVersion) {
+      console.log(`${packageName}@${npmVersion} is up to date`)
+    } else {
       const otp = (otpCache = await new Promise((r) => {
         let message = `Enter OTP code for "${packageName}@${packageJSON.version}"`
         if (otpCache) message += ` (${otpCache})`
